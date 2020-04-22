84e787193121a70685f2bd813414d19e5261ca80
Roberto Padilla <robert.ipo@hotmail.com>
2020-04-21 21:57:06 -0500

    untracking files

--- controllers/products.js
+++ controllers/products.js
@@ -1,42 +1,59 @@
 const Product = require('../models/Product');
 const moment = require('moment');
 
-module.exports = {
-    index: (req, res, next) => {
-        Product.find({active: true}, (err, products) => {
-            if (err) return console.log(err);
-            res.status(200).send({
-                message: "found",
-                data: {
-                    products: products
-                }
-            });
-        });
-    },
-    
-    store: (req, res, next) => {
-        console.log(req.body);
-        const body = req.body;
-        const product = new Product({
-            name: body.name,
-            quantity: body.quantity,
-            price: body.price,
-            color: body.color,
-            description: body.description,
-            measure_id: body.measure_id,
-            created_by: body.created_by,
-        });
-        
-        product.save((err, product) => {
-            if (err) return console.error(err);
-            
-            res.status(201).send({
-                message: "created",
-                data: {
-                    product: product
-                }
-            });
-        });
+const index = (req, res, next) => {
+    Product.find({ active: true }, (err, products) => {
+        if (err) {
+            return res.status(500)
+                .json({
+                    err
+                });
+        };
 
-    }
+        res.status(200).json({
+            message: "found",
+            data: {
+                products
+            }
+        });
+    });
+}
+
+const store = (req, res, next) => {
+    const body = req.body;
+    const product = new Product({
+        name: body.name,
+        quantity: body.quantity,
+        price: body.price,
+        color: body.color,
+        description: body.description,
+        measure_id: body.measure_id,
+        created_by: body.created_by,
+    });
+
+    product.save((err, product) => {
+        if (err) {
+            return res.status(500).json({
+                err
+            });
+        }
+
+        res.status(201).json({
+            message: "created",
+            data: {
+                product
+            }
+        });
+    });
+}
+
+const show = (req, res, next) => {
+    let id = req.params.id;
+
+    Product.findById
+}
+
+module.exports = {
+    index,
+    store
 }
