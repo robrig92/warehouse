389fd54a203a075ecefd093f9f95d51beb334a8c
Roberto Padilla <robert.ipo@hotmail.com>
2020-04-21 22:43:10 -0500

    added products CRUD

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,41 @@
 const Product = require('../models/Product');
-const moment = require('moment');
 
 const index = (req, res, next) => {
-    Product.find({ active: true }, (err, products) => {
-        if (err) {
-            return res.status(500)
-                .json({
-                    err
-                });
-        };
+    let limit = req.query.limit || 10;
+    let offset = req.query.page || 0;
+    let condition = { active: true };
 
-        res.status(200).json({
-            message: "found",
-            data: {
-                products
-            }
+    limit = Number(limit);
+    offset = Number(offset);
+
+    Product.find(condition)
+        .skip(offset)
+        .limit(limit)
+        .exec((err, products) => {
+            if (err) {
+                return res.status(500)
+                    .json({
+                        err
+                    });
+            };
+
+            Product.count(condition, (err, count) => {
+                if (err) {
+                    return res.status(500)
+                        .json({
+                            err
+                        });
+                };
+
+                res.status(200).json({
+                    message: "found",
+                    data: {
+                        count,
+                        products
+                    }
+                });
+            });
         });
-    });
 }
 
 const store = (req, res, next) => {
@@ -51,10 +70,97 @@ const store = (req, res, next) => {
 const show = (req, res, next) => {
     let id = req.params.id;
 
+    Product.findById(id, (err, product) => {
+        if (err) {
+            return res.status(500)
+                .json({
+                    err
+                });
+        }
 
+        if (!product) {
+            return res.status(404)
+                .json({
+                    message: 'Resource not found'
+                });
+        }
+
+        res.json({
+            message: 'Resource found',
+            data: {
+                product
+            }
+        });
+    });
+}
+
+const update = (req, res, next) => {
+    let id = req.params.id;
+    let body = req.body;
+    let args = {
+        name: body.name,
+        price: body.price,
+        color: body.color,
+        description: body.description,
+        measure: body.measure,
+        updated_at: Date.now()
+    };
+
+    Product.findByIdAndUpdate(id, args, { new: true, runValidators: true }, (err, product) => {
+        if (err) {
+            return res.status(500)
+                .json({
+                    err
+                });
+        }
+
+        if (!product) {
+            return res.status(404)
+                .json({
+                    message: 'Resource not found'
+                });
+        }
+
+        return res.json({
+            message: 'Updated',
+            data: {
+                product
+            }
+        });
+    });
+}
+
+const destroy = (req, res, next) => {
+    let id = req.params.id;
+
+    Product.findByIdAndUpdate(id, { active: false }, { new: true }, (err, product) => {
+        if (err) {
+            return res.status(500)
+                .json({
+                    err
+                });
+        }
+
+        if (!product) {
+            return res.status(404)
+                .json({
+                    message: 'Resource not found'
+                });
+        }
+
+        res.json({
+            message: 'Deleted',
+            data: {
+                product
+            }
+        })
+    });
 }
 
 module.exports = {
     index,
-    store
+    store,
+    show,
+    update,
+    destroy
 }
--- routes/products.js
+++ routes/products.js
@@ -5,5 +5,8 @@ var ProductsController = require('../controllers/products');
 
 router.get('/products', ProductsController.index);
 router.post('/products', ProductsController.store);
+router.get('/products/:id', ProductsController.show);
+router.put('/products/:id', ProductsController.update);
+router.delete('/products/:id', ProductsController.destroy);
 
 module.exports = router;
